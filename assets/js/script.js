d873f48b883ef22b8ebd53dc285707377fea0878
Mohamed Adel <134806641+mo-adel99@users.noreply.github.com>
2024-01-22 10:25:44 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 document.querySelectorAll(".answer-toggle").forEach((btn) => {
   btn.addEventListener("click", function() {
     this.nextElementSibling.classList.toggle("answer-active");
-    this.children[1].classList.toggle("icon-active");})})
+    this.children[1].classList.toggle("icon-active");
+})})
